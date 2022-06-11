c4c6936ddbca57072a6d5e922e1aeb752ae55056
AshokrajOwshith <ashokraj110796@gmail.com>
2022-06-12 00:22:58 +0530

    git_added_facebook_project

---
+++ src/test/java/com/facebookstepdifinition/HooksClass.java
@@ -0,0 +1,23 @@
+package com.facebookstepdifinition;
+
+import com.baseclass.BaseClass;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class HooksClass extends BaseClass
+{
+	@Before
+	public void beforeExecution() 
+	{
+		lanuchBrowser();
+		windowMaximize();
+		loadUrl("https://www.facebook.com/");
+	}
+	@After
+	public void afterExecution() throws InterruptedException
+	{
+		Thread.sleep(3000);
+		quitBrowser();
+	}
+}
